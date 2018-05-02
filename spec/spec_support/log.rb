ad254be9b5712800f68c098d3cd7da3ae09c2ab8
Michael Hale <mhale@heroku.com>
2018-05-02 17:58:40 -0400

    silence canonical log line during testing

--- spec/spec_support/log.rb
+++ spec/spec_support/log.rb
@@ -4,6 +4,14 @@ unless ENV["TEST_LOGS"] == "true"
       def log(_data, &block)
         yield if block
       end
+
+      def log_with_default_context(_data, &block)
+        yield if block
+      end
+
+      def log_without_context(_data, &block)
+        yield if block
+      end
     end
   end
 end
